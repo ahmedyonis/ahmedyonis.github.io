785ee0b04294245834876af5d61affc3b0b10843
ahmedyonis <ahmedahmed800@gmail.com>
2026-05-02 10:32:55 +0300

    adding info

--- script.js
+++ script.js
@@ -54,3 +54,63 @@ navButtons.forEach((button) => {
         pageFlip.flip(targetPage);
     });
 });
+
+
+const names = ["Angular", "Laravel", "MySQL"]; 
+let nameIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+let isHovered = false; 
+
+const textElement = document.getElementById("text");
+const container = document.getElementById("text-container");
+
+const TYPE_SPEED = 150;
+const DELETE_SPEED = 100;
+const PAUSE_AFTER_WRITE = 2000;
+
+function type() {
+    const currentName = names[nameIndex];
+
+    if (isDeleting) {
+        textElement.textContent = currentName.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        textElement.textContent = currentName.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    let speed = isDeleting ? DELETE_SPEED : TYPE_SPEED;
+
+
+    if (!isDeleting && charIndex === currentName.length) {
+        if (isHovered) {
+            return;
+        }
+        speed = PAUSE_AFTER_WRITE;
+        isDeleting = true;
+    }
+
+    else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        nameIndex = (nameIndex + 1) % names.length; 
+    }
+
+    setTimeout(type, speed);
+}
+
+
+container.addEventListener("mouseenter", () => {
+    isHovered = true;
+});
+
+container.addEventListener("mouseleave", () => {
+    isHovered = false;
+    // لو كان واقف بعد ما كتب الاسم، نكمل دورة المسح فوراً
+    if (!isDeleting && charIndex === names[nameIndex].length) {
+        isDeleting = true;
+        type();
+    }
+});
+
+type();
